5ceb52d773e9bd829959ae58bbae3c95f396e12a
AdamGeniusDev <adamridwane02@gmail.com>
2025-04-15 20:00:21 +0100

    fix the check

--- components/BookOverview.tsx
+++ components/BookOverview.tsx
@@ -25,10 +25,9 @@ const BookOverview = async({
   userId
 }: Props) => {
   const [user] = await db.select().from(users).where(eq(users.id,userId)).limit(1);
-  if(!user) return null;
 
   const borrowingEligibility = {
-    isEligible : availableCopies > 0 && user.status === 'APPROVED',
+    isEligible : availableCopies > 0 && user?.status === 'APPROVED',
     message: availableCopies <= 0 ?'Book is not available' : 'You are not eligible to borrow this book'
   }
   return (
@@ -57,7 +56,7 @@ const BookOverview = async({
               </p>
             </div>
            <p className="book-description">{description}</p>
-           <BorrowBook bookId={id} userId={userId} borrowingEligibility={borrowingEligibility}/>
+           {user &&<BorrowBook bookId={id} userId={userId} borrowingEligibility={borrowingEligibility}/>}
           </div>
         </div>
         <div className="relative flex flex-1 justify-center">
